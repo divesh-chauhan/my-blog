5281e00e1e27da4afbdbc24b896e277a985bd299
divesh chauhan <diveshc207@gmail.com>
2025-01-19 12:16:17 +0530

    bug-fix cut all code

--- server.js
+++ server.js
@@ -1,4 +1,26 @@
-const app = require('./app');
+require('dotenv').config();
 const PORT = process.env.PORT || 1000;
 
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const route = require('./routes/blogRoute');
+const connect_DB = require('./config/connect_DB');
+
+const app = express();
+connect_DB();
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'))
+app.use(morgan('dev'));
+
+
+app.use('/api', route);
+
 app.listen(PORT, () => console.log(`http://localhost:${PORT}.`));
+
+
